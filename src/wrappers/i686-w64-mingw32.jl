b0aa9cc4a5aaf7e2dd891985af5ce02e006212d2
jlbuild <juliabuildbot@gmail.com>
2021-12-18 18:59:45 +0000

    Libepoxy_jll build 1.5.8+1

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -2,11 +2,10 @@
 export libepoxy
 
 using Libglvnd_jll
-using Xorg_libX11_jll
 JLLWrappers.@generate_wrapper_header("Libepoxy")
 JLLWrappers.@declare_library_product(libepoxy, "libepoxy-0.dll")
 function __init__()
-    JLLWrappers.@generate_init_header(Libglvnd_jll, Xorg_libX11_jll)
+    JLLWrappers.@generate_init_header(Libglvnd_jll)
     JLLWrappers.@init_library_product(
         libepoxy,
         "bin\\libepoxy-0.dll",
--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -2,11 +2,10 @@
 export libepoxy
 
 using Libglvnd_jll
-using Xorg_libX11_jll
 JLLWrappers.@generate_wrapper_header("Libepoxy")
 JLLWrappers.@declare_library_product(libepoxy, "@rpath/libepoxy.0.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(Libglvnd_jll, Xorg_libX11_jll)
+    JLLWrappers.@generate_init_header(Libglvnd_jll)
     JLLWrappers.@init_library_product(
         libepoxy,
         "lib/libepoxy.0.dylib",
